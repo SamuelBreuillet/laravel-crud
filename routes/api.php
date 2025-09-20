43dea3b0cd5701a41975571cb36b56baf70ce5a2
SamuelBreuillet <breuillet.samuel@gmail.com>
2025-09-20 16:07:22 +0200

    feat: add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/posts', PostController::class .'@index')->name('posts.index');
+Route::post('/posts', PostController::class .'@store')->name('posts.store');
+Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
+Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
