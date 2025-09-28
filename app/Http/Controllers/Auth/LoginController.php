534ce9e5d99467154be4ed4946473d3e66d50dd0
SamuelBreuillet <breuillet.samuel@gmail.com>
2025-09-28 12:57:04 +0200

    feat: add login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function __invoke(LoginRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $user = User::query()
+            ->where('email', $validated['email'])
+            ->first();
+
+        abort_if(!$user || !Hash::check($validated['password'], $user->password), Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $token = $user->createToken(
+            config('app.name')
+        );
+
+        return response()->json([
+            'token' => $token->plainTextToken
+        ]);
+    }
+}
